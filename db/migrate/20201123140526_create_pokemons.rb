b339c4f19bfa286c6e9c14ae00ab89d9bed42223
jonasmuecke <jonas@muecke.com>
2020-11-23 15:27:17 +0100

    changes

--- db/migrate/20201123140526_create_pokemons.rb
+++ db/migrate/20201123140526_create_pokemons.rb
@@ -5,7 +5,7 @@ class CreatePokemons < ActiveRecord::Migration[6.0]
       t.string :description
       t.string :type
       t.integer :price_per_day
-      t.User :references
+      t.references :user
 
       t.timestamps
     end
