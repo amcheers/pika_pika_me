3e5315bd4d69af1036fa54e75c81234c2b71cdfb
Ahmad Cheers <acheers1@gmail.com>
2020-11-23 15:50:32 +0100

    Created booking model, added validations, dependency: :destroy for user model

--- db/migrate/20201123140526_create_pokemons.rb
+++ db/migrate/20201123140526_create_pokemons.rb
@@ -5,7 +5,7 @@ class CreatePokemons < ActiveRecord::Migration[6.0]
       t.string :description
       t.string :type
       t.integer :price_per_day
-      t.references :user
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_140526) do
+ActiveRecord::Schema.define(version: 2020_11_23_143913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "pokemon_id", null: false
+    t.bigint "user_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "total_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pokemon_id"], name: "index_bookings_on_pokemon_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.string "type"
     t.integer "price_per_day"
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_pokemons_on_user_id"
@@ -38,4 +50,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_140526) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "pokemons"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "pokemons", "users"
 end
