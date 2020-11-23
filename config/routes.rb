0f1191fe4f49683cebec26e77883d566abac2a4f
Ahmad Cheers <acheers1@gmail.com>
2020-11-23 16:46:21 +0100

    Removed bookings#new route and will place bookings#new in pokemon show.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/dashboard', to: 'pages#dashboard'
   resources :pokemons do 
-    resources :bookings, only: [:new, :create, :destroy], shallow: true
+    resources :bookings, only: [:create, :destroy], shallow: true
   end
 end
