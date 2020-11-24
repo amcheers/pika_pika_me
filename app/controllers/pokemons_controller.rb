a9e3ba059fa9df6f73ab2f042a1657974f3d0a7a
Ahmad Cheers <acheers1@gmail.com>
2020-11-24 11:43:25 +0100

    Coded pokemons#show, coded pokemons#show view, updated components to include css classes used in show, updated user model with address column.

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,4 +2,14 @@ class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all
   end
+
+  def show
+    set_pokemon
+  end
+
+  private
+
+  def set_pokemon
+    @pokemon = Pokemon.find(params[:id])
+  end
 end
